819a27dfef4e9d54972ca588014511352bbf0e5c
Maolin Ding <62633951+DingMaolinBio@users.noreply.github.com>
2024-01-15 16:54:12 +0800

    Update run_predict.sh

--- run_predict.sh
+++ run_predict.sh
@@ -60,7 +60,7 @@ cat $WORKING_DIR/$outdir/${prefix}${option}.output.txt | grep -v "#" | sed 's/_/
 
 
 ## Ensemble
-$WORKING_DIR/src/ensemble/prepare_ensemble_scores.py --ref_dir $ensemble_dir -i $WORKING_DIR/$outdir/${prefix}${option}.output.txt
+$WORKING_DIR/src/prepare_ensemble_scores.py --ref_dir $ensemble_dir -i $WORKING_DIR/$outdir/${prefix}${option}.output.txt
 
 # CADD
 cat $WORKING_DIR/$outdir/${prefix}${option}.output.txt.CADD | sed 's/_/\t/g' | sed 's/^chrX/chr23/g' | sed 's/^chrY/chr24/g' | sed 's/^chr//g' | sort -k1,1n -k2,2n -k3,3 -k4,4 | sed 's/^/chr/g' | sed 's/^chr23/chrX/g' | sed 's/^chr24/chrY/g' | awk '{printf("%s_%s_%s_%s\t%s\n", $1,$2,$3,$4,$5)}' > $WORKING_DIR/$outdir/${prefix}${option}.output.txt.CADD.sorted
@@ -76,10 +76,10 @@ cat $WORKING_DIR/$outdir/${prefix}${option}.output.txt.AlphaMissense | sed 's/_/
 
 paste $WORKING_DIR/$outdir/${prefix}${option}.output.txt.mlgvp.sorted $WORKING_DIR/$outdir/${prefix}${option}.output.txt.CADD.sorted $WORKING_DIR/$outdir/${prefix}${option}.output.txt.MVP.sorted $WORKING_DIR/$outdir/${prefix}${option}.output.txt.MCAP.sorted $WORKING_DIR/$outdir/${prefix}${option}.output.txt.PrimateAI.sorted $WORKING_DIR/$outdir/${prefix}${option}.output.txt.AlphaMissense.sorted | cut -f 1-3,5,7,9,11,13| awk '{printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1,$2,$4,$5,$6,$7,$8,$3)}' > $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt
 
-$WORKING_DIR/src/ensemble/txt_addheader.py -i $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt -o $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt.extend
+$WORKING_DIR/src/txt_addheader.py -i $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt -o $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt.extend
 
 echo "The ML-GVP+ scores were provided in $WORKING_DIR/$outdir/${prefix}${option}.meta.output.txt"
-$WORKING_DIR/src/ensemble/metapredict.py \
+$WORKING_DIR/src/metapredict.py \
     -i $WORKING_DIR/$outdir/${prefix}${option}.combined.output.txt.extend \
     -o $WORKING_DIR/$outdir/${prefix}${option}.meta.output.txt \
     -m $WORKING_DIR/models/final_ensemble.v1.model.pkl
